7087abc07463f2f22ddb560e185cf86b42fd9e13
Faizan Ahemad <ahemf@amazon.com>
2021-01-22 19:36:08 +0530

    bug fix

--- fastformer/config/fastformer_config.py
+++ fastformer/config/fastformer_config.py
@@ -370,7 +370,7 @@ md_config_funnel = FastFormerConfig(separate_content_and_position_attention=Fals
                                                                      # (1, 1), (1, 2), (1, 3), (1, 4),
                                                                      # (2, 1), (2, 2), (2, 3), (2, 4)
                                                                      ],
-                                    n_head=[(4, 4, 0), (12, 0, 0), (16, 0, 0)],
+                                    n_head=[(4, 0, 4), (12, 0, 0), (16, 0, 0)],
                                     block_channel_size=[384, 768, 1280], no_v_head=False, expand_dim_before_pooling=True, char_rnn=True, char_rnn_window_overlap=32, char_rnn_window_size=128,
                                     short_rnn=[True, False, False], short_rnn_overlap=[16, 0, 0], short_rnn_kernel=[16, 0, 0],
                                     )
