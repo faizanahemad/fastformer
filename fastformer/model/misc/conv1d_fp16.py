dcbcbbdad4526425eb9849ab1d077e4393094eab
Faizan Ahemad <ahemf@amazon.com>
2021-01-29 15:10:18 +0530

    Bug fix

--- fastformer/model/misc/conv1d_fp16.py
+++ fastformer/model/misc/conv1d_fp16.py
@@ -81,6 +81,56 @@ class SeparableConv1d(nn.Module):
         return inputs
 
 
+class SeparableConv1dV2(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, groups=None, pointwise_groups=None,
+                 bias=True, stride=1, padding=None):
+        super().__init__()
+        if padding is None:
+            padding = (kernel_size - 1) // 2
+        if groups is None:
+            groups = in_channels
+        self.depthwise = nn.Conv1d(in_channels=in_channels, out_channels=in_channels,
+                                   kernel_size=kernel_size, groups=groups, bias=False, stride=stride, padding=padding)
+        self.pointwise = nn.Linear(in_channels=in_channels, out_channels=out_channels, bias=bias)
+
+        self.out_channels = out_channels
+
+    def forward(self, inputs):
+        """
+        Expect inputs in channels first format
+        :param inputs:
+        :return:
+        """
+        inputs = inputs.permute(0, 2, 1)
+        inputs = self.depthwise(inputs)
+        inputs = inputs.permute(0, 2, 1)
+        inputs = self.pointwise(inputs)
+        return inputs
+
+
+class SeparableConv1dV3(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, groups=None, pointwise_groups=None,
+                 bias=True, stride=1, padding=None):
+        super().__init__()
+        if padding is None:
+            padding = (kernel_size - 1) // 2
+        self.depthwise = nn.Conv1d(in_channels=in_channels, out_channels=in_channels,
+                                   kernel_size=kernel_size, groups=1, bias=False, stride=stride, padding=padding)
+
+        self.out_channels = out_channels
+
+    def forward(self, inputs):
+        """
+        Expect inputs in channels first format
+        :param inputs:
+        :return:
+        """
+        inputs = inputs.permute(0, 2, 1)
+        inputs = self.depthwise(inputs)
+        inputs = inputs.permute(0, 2, 1)
+        return inputs
+
+
 if __name__ == "__main__":
     import time
     import argparse
@@ -117,6 +167,10 @@ if __name__ == "__main__":
         model = Conv1d(channels, channels, kernel, groups).to(device)
     elif model == "sep":
         model = SeparableConv1d(channels, channels, kernel, pointwise_groups=groups, stride=stride).to(device)
+    elif model == "sep_v2":
+        model = SeparableConv1dV2(channels, channels, kernel, pointwise_groups=groups, stride=stride).to(device)
+    elif model == "sep_v3":
+        model = SeparableConv1dV3(channels, channels, kernel, pointwise_groups=groups, stride=stride).to(device)
     else:
         raise ValueError
 
