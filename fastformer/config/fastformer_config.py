e17c45611eefd0cf0d2f14614d84a6a42af04dd6
Faizan Ahemad <ahemf@amazon.com>
2021-01-29 16:42:22 +0530

    Bug fix

--- fastformer/config/fastformer_config.py
+++ fastformer/config/fastformer_config.py
@@ -416,7 +416,7 @@ md_config_rnn = FastFormerConfig(separate_content_and_position_attention=False, 
                                  short_rnn=[True, False, False], short_rnn_overlap=[16, 0, 0], short_rnn_kernel=[32, 0, 0],
                                  )
 
-md_config_funnel = FastFormerConfig(separate_content_and_position_attention=False, pooling_type="mean", pooling_kernel_size=7, use_cuda_conv=False,
+md_config_funnel = FastFormerConfig(separate_content_and_position_attention=False, pooling_type="mean", pooling_kernel_size=7, use_cuda_conv=True,
                                     sequence_dependent_position_transform=False, stride=4, qkv_transform_groups=8, ffn_groups=8,
                                     approximate_attention=[False, False, False], max_position_embeddings=1152, d_head=[48, 64, 80],
                                     separate_compressiion_layer=True, qkv_squeeze_fraction=1, light_last_layer=False, light_first_layer=False,
@@ -500,7 +500,7 @@ md_config_funnel_lp = FastFormerConfig(separate_content_and_position_attention=F
                                        )
 
 
-md_config_funnel_sp = FastFormerConfig(separate_content_and_position_attention=False, pooling_type="learn_sdconv", pooling_kernel_size=7, use_cuda_conv=False,
+md_config_funnel_sp = FastFormerConfig(separate_content_and_position_attention=False, pooling_type="learn_sdconv", pooling_kernel_size=7, use_cuda_conv=True,
                                        sequence_dependent_position_transform=False, stride=4, qkv_transform_groups=1, ffn_groups=1,
                                        approximate_attention=[False, False, False], max_position_embeddings=1152, d_head=[48, 64, 80],
                                        separate_compressiion_layer=True, qkv_squeeze_fraction=1, light_last_layer=False, light_first_layer=False,
