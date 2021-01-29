83bba32b55610629a7b86b9abb58d4b3bcbeb447
Faizan Ahemad <ahemf@amazon.com>
2021-01-29 22:57:56 +0530

    Bug fix

--- fastformer/model/fastformer_model.py
+++ fastformer/model/fastformer_model.py
@@ -1251,10 +1251,16 @@ class TransformerLayer(nn.Module):
         self.attention = MultiheadAttention(config, block_index, is_last_layer_of_block, is_first_layer_of_block, is_encoder_layer, layer_index,
                                             last_layer_index)
         self.ffn = PositionwiseFFN(config, block_index, is_last_layer_of_block, is_encoder_layer)
+        self.alternate_ffn = config.alternate_ffn if hasattr(config, "alternate_ffn") else True
+        self.block_index = block_index
 
     def forward(self, query, key, value, attention_inputs, layer_index, output_attentions=False):
         attn = self.attention(query, key, value, attention_inputs, layer_index, output_attentions=output_attentions)
-        pre_ffn, output = self.ffn(attn[0], layer_index)
+        pre_ffn, output = attn[0], attn[0]
+        if self.alternate_ffn and not is_last_layer_of_block and layer_index % 2 == 0:
+            pass
+        else:
+            pre_ffn, output = self.ffn(attn[0], layer_index)
         return (output, pre_ffn, attn[1]) if output_attentions else (output, pre_ffn)
 
 
