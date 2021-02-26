5f6ad56230d94ecb985e560fb0bf6331cbd65267
Faizan Ahemad <ahemf@amazon.com>
2021-02-26 14:01:44 +0530

    Bug fix

--- fastformer/training/pickle4reducer.py
+++ fastformer/training/pickle4reducer.py
@@ -12,7 +12,6 @@ class ForkingPickler4(pickle.Pickler):
         print(type(file), type(protocol), file)
         buf = io.BytesIO()
         pickle.Pickler(buf, protocol).dump(file)
-        file = buf.getbuffer()
         super().__init__(file, protocol, buffer_callback=buffer_callback)
         self.dispatch_table = self._copyreg_dispatch_table.copy()
         self.dispatch_table.update(self._extra_reducers)
