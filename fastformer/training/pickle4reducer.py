1e8074ab6849b9c9dc810077f3265e1ffa1cc8f0
Faizan Ahemad <ahemf@amazon.com>
2021-02-26 12:03:13 +0530

    Bug fix

--- fastformer/training/pickle4reducer.py
+++ fastformer/training/pickle4reducer.py
@@ -4,11 +4,18 @@ import pickle
 
 class ForkingPickler4(ForkingPickler):
     def __init__(self, *args):
+        args = list(args)
+        if len(args) > 1:
+            args[1] = pickle.HIGHEST_PROTOCOL
+        else:
+            args.append(pickle.HIGHEST_PROTOCOL)
         super().__init__(*args)
 
     @classmethod
     def dumps(cls, obj, protocol=pickle.HIGHEST_PROTOCOL):
-        return ForkingPickler.dumps(obj, protocol)
+        return ForkingPickler.dumps(obj, pickle.HIGHEST_PROTOCOL)
+
+    loads = pickle.loads
 
 
 def dump(obj, file, protocol=pickle.HIGHEST_PROTOCOL):
