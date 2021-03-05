66e15c7f546ca2dae02aaaf733d4769f8566601d
Faizan Ahemad <ahemf@amazon.com>
2021-03-05 11:32:30 +0530

    Bug fix

--- fastformer/config/fastformer_config.py
+++ fastformer/config/fastformer_config.py
@@ -14,7 +14,7 @@ from dataclasses_json import dataclass_json
 
 # size_dicts_val = {128:112, 256:64, 512: 32, 768: 16, 1024: 16}
 # size_dicts = {128:96, 256:48, 512: 24, 768: 16, 1024: 8}
-size_dicts = {128:80, 256:40, 512: 16, 768: 16, 1024: 8}
+size_dicts = {128:80, 256:24, 512: 16, 768: 16, 1024: 8}
 
 
 @dataclass_json
