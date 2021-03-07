8a95bf596c9566404f28edbd3524ddbcd6c600d7
Faizan Ahemad <ahemf@amazon.com>
2021-03-07 23:36:08 +0530

    Add extra LN for stability

--- fastformer/config/fastformer_config.py
+++ fastformer/config/fastformer_config.py
@@ -33,8 +33,8 @@ class OptimizerConfig:
     sampling_fraction: float
 
 
-optimizer_config = OptimizerConfig(1e-5, 1e-7, 1e-2, 0.9, 0.98,
-                                   8, 8, 10000, 0.1, 0.5)
+optimizer_config = OptimizerConfig(5e-5, 1e-7, 1e-2, 0.9, 0.98,
+                                   8, 8, 10000, 1.0, 0.5)
 
 
 @dataclass_json
