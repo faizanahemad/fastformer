404377c0cdb44d9b551e3cf933710e19f6998694
Faizan Ahemad <ahemf@amazon.com>
2021-03-13 18:29:30 +0530

    Strict grad clipping with values

--- fastformer/orchestrate/run_on_hosts.py
+++ fastformer/orchestrate/run_on_hosts.py
@@ -122,8 +122,8 @@ if __name__ == "__main__":
     main_cmd = """python train_lm_distributed.py -n %s -g 8 --nr %s --model_config md_config"""
     main_cmd += " --model_save_dir /home/ahemf/model_save_dir --model_save_name fastformer.pth"
 
-    main_cmd += " --train_dataset /home/ahemf/processed_datasets/train_fastformer_resampled_10M"
-    # main_cmd += " --train_dataset /home/ahemf/processed_datasets/train_fastformer_resampled_50M"
+    # main_cmd += " --train_dataset /home/ahemf/processed_datasets/train_fastformer_resampled_10M"
+    main_cmd += " --train_dataset /home/ahemf/processed_datasets/train_fastformer_resampled_50M"
 
     main_cmd += " --validation_dataset /home/ahemf/processed_datasets/validation_fastformer"
     main_cmd += " --log_every_steps 50 --num_workers 32 --validate_every_steps 80000 --save_every_steps 1000"
@@ -141,9 +141,9 @@ if __name__ == "__main__":
     main_cmd += " --checkpoint /home/ahemf/torch_distributed_init/fastformer_checkpoint"
 
     # main_cmd += " --cpu"
-    main_cmd += " --no_autocast"
+    # main_cmd += " --no_autocast"
     # main_cmd += " --detect_anomaly"
-    # main_cmd += " --shuffle_dataset"
+    main_cmd += " --shuffle_dataset"
 
     main_cmd += " > output.log 2>&1 & disown"
 
