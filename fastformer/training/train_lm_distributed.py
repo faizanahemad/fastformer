0d6f4a1d25ac219d029a9b4be88ed43c6dca66ed
Faizan Ahemad <ahemf@amazon.com>
2021-03-16 20:24:33 +0530

    Change batching

--- fastformer/training/train_lm_distributed.py
+++ fastformer/training/train_lm_distributed.py
@@ -465,10 +465,10 @@ def train(local_rank, args):
     os.environ['MASTER_ADDR'] = args["master_addr"]
     os.environ['MASTER_PORT'] = args["master_port"]
     os.environ["NCCL_DEBUG"] = "INFO"
-    os.environ["CUDA_VISIBLE_DEVICES"] = str(local_rank)
-    gpu_device = 0
-    # gpu_device = local_rank
-    # os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    # os.environ["CUDA_VISIBLE_DEVICES"] = str(local_rank)
+    # gpu_device = 0
+    gpu_device = local_rank
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
     if args["wandb_dryrun"]:
         os.environ["WANDB_MODE"] = "dryrun"
         os.environ["WANDB_SILENT"] = "true"
