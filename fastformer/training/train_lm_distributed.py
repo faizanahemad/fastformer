45d66d208558c19ffbd183f6a29c708474868084
Faizan Ahemad <ahemf@amazon.com>
2021-03-16 12:13:57 +0530

    Wandb on local_rank = 0 only

--- fastformer/training/train_lm_distributed.py
+++ fastformer/training/train_lm_distributed.py
@@ -192,7 +192,7 @@ class SuperGLUEValidator:
 
 
 class LargeValidator:
-    def __init__(self, location, model, config, device, tokenizer, rank, world_size, local_rank, wandb_init_args, no_autocast=False):
+    def __init__(self, location, model, config, device, tokenizer, rank, world_size, no_autocast=False):
         self.location = location
         self.model = model
         self.config = config
@@ -257,8 +257,6 @@ class LargeValidator:
                          'swag_qna'
                          ]
         self.no_autocast = no_autocast
-        self.local_rank = local_rank
-        self.wandb_init_args = wandb_init_args
 
     def __call__(self):
         # TODO: save model if val acc higher than before
@@ -357,7 +355,9 @@ class LargeValidator:
                 results[k] = pd.DataFrame.from_records(predictions).mean().to_dict()
                 _ = results[k].pop("answering_lm_accuracy", None)
             print("[Validation]: Time = %s, Rank = %s, Finished-Validation, For Dataset %s, results = %s" % (get_time_string(), self.rank, k, results[k]))
-            wandb.log(dict(k=results[k]), commit=False)
+            with open('validation.txt', 'a') as f:
+                print(str(dict(k=results[k])) + os.linesep, file=f)
+
             clean_memory()
         model = model.train()
         return results
@@ -493,12 +493,13 @@ def train(local_rank, args):
     optimizer = AdamW(all_params, lr=optc["lr"], eps=optc["eps"], weight_decay=optc["weight_decay"], betas=(optc["beta_1"], optc["beta_2"]))
     optimizer.zero_grad()
     scaler = GradScaler()
-    wandb_init_args = dict(project="fastformer", name="%s-%s-%s-%s" % (group, args["nr"], rank, local_rank), group=group, id=f"{group}-worker-{nr}-{rank}-{local_rank}",
-                           config={"args":args, "model_config": mconf, "config": config, "optimizer_config": optc},
-                           settings=wandb.Settings(start_method="fork"))
-    
-    time.sleep(random.random() * 5)
-    wandb.init(**wandb_init_args)
+    if local_rank == 0:
+        wandb_init_args = dict(project="fastformer", name="%s-%s-%s-%s" % (group, args["nr"], rank, local_rank), group=group, id=f"{group}-worker-{nr}-{rank}-{local_rank}",
+                               config={"args":args, "model_config": mconf, "config": config, "optimizer_config": optc},
+                               settings=wandb.Settings(start_method="fork"))
+
+        time.sleep(random.random() * 5)
+        wandb.init(**wandb_init_args)
     # model, optim, gradscaler, scheduler, steps
 
     model_save_dir = args["model_save_dir"]
@@ -535,7 +536,7 @@ def train(local_rank, args):
         print("[Train]: No Resume for Rank = %s" % rank)
     _ = model.train()
     if args["validate_on_start"] or args["validate_only"]:
-        _ = LargeValidator(args["validation_dataset"], ddp_model, config, device, tokenizer, rank, args["world_size"], local_rank, wandb_init_args, args["no_autocast"])()
+        _ = LargeValidator(args["validation_dataset"], ddp_model, config, device, tokenizer, rank, args["world_size"], args["no_autocast"])()
         if args["validate_only"]:
             return
     print("[Train]: Init Wandb-watch added over model for Rank = %s" % rank)
@@ -607,7 +608,7 @@ def train(local_rank, args):
                 if "checkpoint" in args and isinstance(args["checkpoint"], str) and len(args["checkpoint"].strip()) > 0:
                     save(args["checkpoint"], ddp_model, optimizer, scheduler, scaler, {"step": step, "samples_processed": samples_processed, "world_size": args["world_size"]})
         if (step + 1) % validate_every_steps == 0:
-            _ = LargeValidator(args["validation_dataset"], ddp_model, config, device, tokenizer, rank, args["world_size"], local_rank, wandb_init_args, args["no_autocast"])()
+            _ = LargeValidator(args["validation_dataset"], ddp_model, config, device, tokenizer, rank, args["world_size"], args["no_autocast"])()
             barrier()
         record_accuracy = False
         if (step + 1) % log_every_steps == 0:
@@ -682,10 +683,10 @@ def train(local_rank, args):
             samples_per_second = samples_processed_this_log_iter / np.sum(full_times)
             acc_dict = output["accuracy_hist"]
             time.sleep(random.random() + 0.1)
-            wandb.log(dict(lr=optimizer.param_groups[0]['lr'], step=step, samples_processed=samples_processed, samples_per_second=samples_per_second, batch_x_sequence=np.prod(bs[:2]),
-                           batch_times=np.mean(batch_times), model_times=np.mean(model_times), full_times=np.mean(full_times), scale=scaler.get_scale(),
-                           **loss_dict, **acc_dict))
             if local_rank == 0:
+                wandb.log(dict(lr=optimizer.param_groups[0]['lr'], step=step, samples_processed=samples_processed, samples_per_second=samples_per_second, batch_x_sequence=np.prod(bs[:2]),
+                               batch_times=np.mean(batch_times), model_times=np.mean(model_times), full_times=np.mean(full_times), scale=scaler.get_scale(),
+                               **loss_dict, **acc_dict))
                 print("[Train]: Time = %s, Rank = %s, steps = %s, samples_processed=%s, scale = %s, batch_size = %s, Loss = %s, Accuracy = %s, LR = %s" %
                       (get_time_string(), rank, step, samples_processed, scaler.get_scale(),
                        bs_size, loss_dict, output["accuracy_hist"], optimizer.param_groups[0]['lr']))
@@ -711,14 +712,7 @@ def train(local_rank, args):
             save(args["checkpoint"], ddp_model, optimizer, scheduler, scaler,
                  {"step": step, "samples_processed": samples_processed, "world_size": args["world_size"], "loss": loss_dict, "accuracy_dict": acc_dict})
 
-    # Take inputs to local_rank
-
-    # TODO: validate on multigpu, sort the val datasets alphabetically and let the gpu with rank == dataset rank in sort pick up the dataset. GPUs with rank > len(datasetDict) stay idle.
-    # TODO: select one dataset and make full batch from it, this way rebalancing can be easy.
-    # TODO: dataset rebalancing.
-    # TODO: save model only in local_rank == 0 process
-    # TODO: Check if all initialised model weights are same??
-    # I've been tracking an ema of sample training loss during training and using that to guide weighted data sampling (rather than the typical uniform sampling). Seems to help with a variety of real world datasets where the bulk of the data is often very similar and easy to learn but certain subpopulations are much more challenging.
+# I've been tracking an ema of sample training loss during training and using that to guide weighted data sampling (rather than the typical uniform sampling). Seems to help with a variety of real world datasets where the bulk of the data is often very similar and easy to learn but certain subpopulations are much more challenging.
 
 
 if __name__ == "__main__":
