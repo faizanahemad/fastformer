3e8992d6c1ee92879215be1c60f4dc5ddc92d9d8
Faizan Ahemad <ahemf@amazon.com>
2021-03-21 12:23:34 +0530

    answering model

--- fastformer/data/dataset.py
+++ fastformer/data/dataset.py
@@ -250,7 +250,7 @@ def char_rnn_tokenize(text, tokenizer, char_to_id, **tokenizer_args):
 class TokenizerDataset(Dataset):
     def __init__(self, config: FastFormerConfig, tokenizer: PreTrainedTokenizerFast,
                  char_to_id: dict, tokenizer_args: dict, dataset: Dataset, sentence_jumble_proba=((0, 0.5), (256, 0.5), (512, 0.5), (1024, 0.5)),
-                 word_jumble_proba=((0, 0.0), (128, 0.1), (256, 0.15), (512, 0.2), (1024, 0.25)),
+                 word_jumble_proba=((0, 0.05), (128, 0.1), (256, 0.15), (512, 0.2), (1024, 0.25)),
                  word_mask_in_pet=False, word_noise_in_pet=True, sentence_jumble_in_pet=False, word_jumble_in_pet=True,
                  word_mask_proba: list = ((0, 0.05), (128, 0.1), (256, 0.15), (512, 0.15), (1024, 0.2)),
                  word_noise_proba: tuple = ((0, 0.1), (128, 0.1), (256, 0.1), (512, 0.15), (1024, 0.2)),
