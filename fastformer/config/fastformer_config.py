361f8a036c20cb33cfd7c5d4366db7daa6021910
Faizan Ahemad <ahemf@amazon.com>
2021-03-28 16:43:19 +0530

    Bug fix

--- fastformer/config/fastformer_config.py
+++ fastformer/config/fastformer_config.py
@@ -293,10 +293,10 @@ sm_config = FastFormerConfig(separate_content_and_position_attention=False, pool
 # Fasttest
 md_config = FastFormerConfig(separate_content_and_position_attention=False, pooling_type="learn_sdconv", pooling_kernel_size=5, use_cuda_conv=True,
                              sequence_dependent_position_transform=False, stride=4, qkv_transform_groups=1, ffn_groups=1,
-                             approximate_attention=[False, False, False], max_position_embeddings=1024, d_head=[24, 32, 64], alternate_ffn=True,
-                             separate_compressiion_layer=True, light_last_layer=False, light_first_layer=True,
-                             sdconv=[True, True, False], full_channel_separation=True,
-                             sdconv_kernel_size=[5, 3, 3],
+                             approximate_attention=[False, False, False], max_position_embeddings=1024, d_head=[64, 64, 64], alternate_ffn=False,
+                             separate_compressiion_layer=True, light_last_layer=False, light_first_layer=False,
+                             sdconv=[True, False, False], full_channel_separation=True,
+                             sdconv_kernel_size=[5, 5, 3],
                              compress_query_method=None, compressed_query_attention_stride=2, compressed_query_attention_kernel_size=3,
                              compressed_query_attention_layers=[(0, 3), (0, 4),
                                                                 # (1, 2), (1, 3), (1, 4),
@@ -307,7 +307,7 @@ md_config = FastFormerConfig(separate_content_and_position_attention=False, pool
                                                               # (2, 1), (2, 2), (2, 3), (2, 4)
                                                               ],
                              n_head=[(4, 4, 0), (5, 5, 0), (12, 0, 0)],
-                             block_channel_size=[384, 640, 768], no_v_head=False, expand_dim_before_pooling=False, char_rnn=True, char_rnn_window_overlap=64,
+                             block_channel_size=[512, 640, 768], no_v_head=False, expand_dim_before_pooling=False, char_rnn=True, char_rnn_window_overlap=64,
                              char_rnn_window_size=128,
                              short_rnn=[False, False, False], short_rnn_overlap=[8, 8, 8], short_rnn_kernel=[32, 32, 32],
                              )
--- fastformer/orchestrate/run_on_hosts.py
+++ fastformer/orchestrate/run_on_hosts.py
@@ -133,12 +133,12 @@ if __name__ == "__main__":
     main_cmd = """python train_lm_distributed.py -n %s -g 8 --nr %s --model_config tg_config"""
     main_cmd += " --model_save_dir /home/ahemf/model_save_dir --model_save_name fastformer.pth"
 
-    main_cmd += " --train_dataset /home/ahemf/processed_datasets/train_fastformer_resampled_10M"
-    # main_cmd += " --train_dataset /home/ahemf/processed_datasets/train_fastformer_resampled_50M"
+    # main_cmd += " --train_dataset /home/ahemf/processed_datasets/train_fastformer_resampled_10M"
+    main_cmd += " --train_dataset /home/ahemf/processed_datasets/train_fastformer_resampled_50M"
     # main_cmd += " --train_dataset /home/ahemf/processed_datasets/train_fastformer_resampled_100M"
 
     main_cmd += " --validation_dataset /home/ahemf/processed_datasets/validation_fastformer"
-    main_cmd += " --log_every_steps 50 --num_workers 8 --validate_every_steps 500000 --save_every_steps 5000"
+    main_cmd += " --log_every_steps 100 --num_workers 8 --validate_every_steps 500000 --save_every_steps 4000"
     # main_cmd += " --wandb_dryrun"
 
     # main_cmd += " --init_method=file --master_addr /home/ahemf/torch_distributed_init --master_port file-9999"
@@ -146,7 +146,7 @@ if __name__ == "__main__":
 
     # main_cmd += " --resume /home/ahemf/torch_distributed_init/fastformer_checkpoint"
 
-    # main_cmd += " --pretrained_model /home/ahemf/model_save_dir/fastformer.pth"
+    main_cmd += " --pretrained_model /home/ahemf/model_save_dir/fastformer.pth"
 
     # main_cmd += " --validate_on_start --validate_only"
 
