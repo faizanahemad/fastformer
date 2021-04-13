0271124793c142924df95fcea7b499279feab49b
Faizan Ahemad <ahemf@amazon.com>
2021-04-13 21:57:33 +0530

    Visual Transformer Augs

--- fastformer/config/fastformer_config.py
+++ fastformer/config/fastformer_config.py
@@ -355,7 +355,7 @@ tg_config = FastFormerConfig(separate_content_and_position_attention=False, pool
                              char_rnn_window_size=128,
                              )
 
-vision_base_config = FastFormerConfig(stride=1, d_head=[64, 64], n_head=[(12, 0, 0), (12, 0, 0)], block_channel_size=[768, 768], num_decoder_layers=2, block_sizes=[6, 6], num_highway_cls_tokens=1, has_decoder=False, hidden_dropout=0.0, activation_dropout=0.0)
+vision_base_config = FastFormerConfig(stride=1, d_head=[64, 64], n_head=[(12, 0, 0), (12, 0, 0)], block_channel_size=[768, 768], num_decoder_layers=2, block_sizes=[6, 6], num_highway_cls_tokens=1, has_decoder=False, hidden_dropout=0.0, attention_dropout=0.0)
 
 vision_md_config = FastFormerConfig(stride=1, d_head=[48, 64], n_head=[(8, 0, 0), (12, 0, 0)], block_channel_size=[384, 768], num_decoder_layers=2, block_sizes=[6, 6], num_highway_cls_tokens=8)
 
