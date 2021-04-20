a0c3b39db4a5d741109b57f6c9ede4830d4f86e4
Faizan Ahemad <ahemf@amazon.com>
2021-04-20 21:21:12 +0530

    vision_base_rel_config

--- fastformer/config/fastformer_config.py
+++ fastformer/config/fastformer_config.py
@@ -369,7 +369,7 @@ tg_config = FastFormerConfig(separate_content_and_position_attention=False, pool
 vision_base_config = FastFormerConfig(stride=1, d_head=[64, 64], n_head=[(12, 0, 0), (12, 0, 0)], block_channel_size=[768, 768], num_decoder_layers=2, block_sizes=[6, 6], num_highway_cls_tokens=1, has_decoder=False, hidden_dropout=0.0, attention_dropout=0.0)
 
 vision_base_rel_config = FastFormerConfig(stride=1, d_head=[64, 64], n_head=[(12, 0, 0), (12, 0, 0)], block_channel_size=[768, 768], num_decoder_layers=2, block_sizes=[6, 6], num_highway_cls_tokens=1, has_decoder=False,
-                                          hidden_dropout=0.0, attention_dropout=0.0, max_position_embeddings=14, relative_attention=True)
+                                          hidden_dropout=0.1, attention_dropout=0.1, max_position_embeddings=14, relative_attention=True)
 
 vision_md_config = FastFormerConfig(stride=1, d_head=[48, 64], n_head=[(8, 0, 0), (12, 0, 0)], block_channel_size=[384, 768], num_decoder_layers=2, block_sizes=[4, 6], num_highway_cls_tokens=2)
 
