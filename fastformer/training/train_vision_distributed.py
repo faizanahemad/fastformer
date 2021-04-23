d815b570af51c1ec41c10444c06fe5a95272a2fc
Faizan Ahemad <ahemf@amazon.com>
2021-04-23 18:20:03 +0530

    Stabilise SimCLR

--- fastformer/training/train_vision_distributed.py
+++ fastformer/training/train_vision_distributed.py
@@ -339,10 +339,10 @@ def train(local_rank, args):
     if args["mode"] == "clr":
         if args["deit"]:
             model = PatchCLR(backbone, 768, config.layer_norm_eps, patchclr_w=0.5, simclr_w=2.0, clustering_w=0.0, gap_bias_w=0.0,
-                             reinit=reinit).to(device)
+                             reinit=reinit, patchclr_use_extra_negatives=False, simclr_use_extra_negatives=False).to(device)
         else:
             model = PatchCLR(backbone, config.block_channel_size[0], config.layer_norm_eps, patchclr_w=0.5, simclr_w=2.0, clustering_w=0.0, gap_bias_w=0.0,
-                             reinit=reinit).to(device)
+                             reinit=reinit, patchclr_use_extra_negatives=False, simclr_use_extra_negatives=False).to(device)
     elif args["mode"] in ['linear_probe', 'full_train', 'validation']:
         model = ClassificationModel(backbone, args["num_classes"], 768 if args["deit"] else (config.block_channel_size[0] + config.block_channel_size[1]), train_backbone=True if "full_train" else False,
                                     reinit_backbone=not args["deit"] and not (args["pretrained_model"] is not None and os.path.exists(args["pretrained_model"]))).to(device)
