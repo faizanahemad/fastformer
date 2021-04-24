aecf7b5d5dd22faa771670d40d328c8cca7df343
Faizan Ahemad <ahemf@amazon.com>
2021-04-24 22:07:01 +0530

    Stabilise SimCLR

--- fastformer/model/fastformer_vision_model.py
+++ fastformer/model/fastformer_vision_model.py
@@ -401,7 +401,7 @@ class PatchCLR(FastFormerPreTrainedModel):
     def __init__(self, backbone, num_features=384, eps=1e-4,
                  patchclr_w=1.0, contrastive_temperature=5e-2,
                  simclr_w=1.0, clustering_w=1.0, gap_bias_w=0.1, simclr_use_extra_negatives=True, patchclr_use_extra_negatives=True,
-                 reinit=False, ):
+                 reinit=False, priority_clr=False, moco=False):
         super().__init__(backbone.config if hasattr(backbone, "config") else PretrainedConfig(initializer_std=1.0))
         self.backbone = backbone
         self.loss_ce = CrossEntropyLoss(ignore_index=-100)
@@ -415,7 +415,8 @@ class PatchCLR(FastFormerPreTrainedModel):
         self.gap_bias_w = gap_bias_w
         self.simclr_use_extra_negatives = simclr_use_extra_negatives
         self.patchclr_use_extra_negatives = patchclr_use_extra_negatives
-        self.priority_clr = False
+        self.priority_clr = priority_clr
+        self.moco = moco
         if reinit:
             self.init_weights()
 
