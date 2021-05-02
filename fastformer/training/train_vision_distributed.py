626b09bf358086de7d7433dc7567c8d1fb8caee3
Faizan Ahemad <ahemf@amazon.com>
2021-05-02 19:56:41 +0530

    Less sync

--- fastformer/training/train_vision_distributed.py
+++ fastformer/training/train_vision_distributed.py
@@ -70,9 +70,9 @@ def training_args():
                         help='Batch Size')
     parser.add_argument('--warm_restart_key_encoder', required=False, type=int,
                         help='Warm Restarts Epochs for Key Encoder')
-    parser.add_argument('--total_samples_patchclr', default=8192, type=int,
+    parser.add_argument('--total_samples_patchclr', default=8192 * 2, type=int,
                         help='Patch Negatives')
-    parser.add_argument('--total_samples_simclr', default=8192, type=int,
+    parser.add_argument('--total_samples_simclr', default=8192 * 2, type=int,
                         help='SimCLR Negatives')
 
     parser.add_argument('--warmup_steps', default=optimizer_config.warmup_steps, type=int,
