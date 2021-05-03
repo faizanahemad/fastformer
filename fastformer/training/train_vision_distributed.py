af71bb1f030fb92c249232faa00975d21a8266ca
Faizan Ahemad <ahemf@amazon.com>
2021-05-03 13:42:18 +0530

    Bug fix

--- fastformer/training/train_vision_distributed.py
+++ fastformer/training/train_vision_distributed.py
@@ -70,7 +70,7 @@ def training_args():
                         help='Batch Size')
     parser.add_argument('--warm_restart_key_encoder', required=False, type=int,
                         help='Warm Restarts Epochs for Key Encoder')
-    parser.add_argument('--total_samples_patchclr', default=65536 * 2, type=int,
+    parser.add_argument('--total_samples_patchclr', default=65536, type=int,
                         help='Patch Negatives')
     parser.add_argument('--total_samples_simclr', default=65536 * 2, type=int,
                         help='SimCLR Negatives')
