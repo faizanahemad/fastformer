2c0ebc4bb50da0d15c31fe110c072d0843f0b595
Faizan Ahemad <ahemf@amazon.com>
2021-05-11 18:08:17 +0530

    Bug fix

--- fastformer/training/train_lm_distributed.py
+++ fastformer/training/train_lm_distributed.py
@@ -1334,8 +1334,8 @@ if __name__ == "__main__":
     # torch.multiprocessing.set_sharing_strategy('file_system')
     args = training_args()
     if args["world_size"] == 1 or args["cpu"]:
-        train_catch_exception(0, args)
+        train(0, args)
     else:
-        mp.spawn(train_catch_exception, nprocs=args["gpus_per_node"], args=(args,), join=True)
+        mp.spawn(train, nprocs=args["gpus_per_node"], args=(args,), join=True)
 
 
