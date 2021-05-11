fceb2f9221dbe83a3d9aebb17e10468454603c9c
Faizan Ahemad <ahemf@amazon.com>
2021-05-11 15:21:21 +0530

    Bug fix

--- fastformer/training/train_vision_distributed.py
+++ fastformer/training/train_vision_distributed.py
@@ -527,7 +527,17 @@ def train(local_rank, args):
         for step, batch in enumerate(dataloader):
             steps_done = epoch * len(dataloader) + step
             pct_done = (100 * steps_done / total_steps)
-            discriminator_pos_frac = 0.85 * min(1.0, max(0.01, pct_done / 100.0))
+            discriminator_pos_frac = 0.85 * min(1.0, max(0.02, pct_done / 100.0))
+            generator_w_progressive = generator_w * min(1.0, max(0.001, pct_done / 100.0))
+            discriminator_w_progressive = discriminator_w * min(1.0, max(0.001, pct_done / 100.0))
+            if hasattr(ddp_model, "module"):
+                ddp_model.module.generator_w = generator_w_progressive
+                ddp_model.module.discriminator_w = discriminator_w_progressive
+            else:
+                ddp_model.generator_w = generator_w_progressive
+                ddp_model.discriminator_w = discriminator_w_progressive
+            model.generator_w = generator_w_progressive
+            model.discriminator_w = discriminator_w_progressive
             if isinstance(batch, dict):
                 key = list(batch.keys())[0]
                 bs_size = list(batch[key].size())
