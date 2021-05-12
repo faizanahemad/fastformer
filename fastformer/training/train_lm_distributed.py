064c7fe4af4c340b165f294bfaf970d2a110d8ed
Faizan Ahemad <ahemf@amazon.com>
2021-05-12 18:04:04 +0530

    Bug fix

--- fastformer/training/train_lm_distributed.py
+++ fastformer/training/train_lm_distributed.py
@@ -1387,7 +1387,7 @@ if __name__ == "__main__":
     if args["world_size"] == 1 or args["cpu"]:
         train_catch_exception(0, args)
     else:
-        start_processes(train_catch_exception, (args,), args["gpus_per_node"], True, False, start_method='spawn')
-        # mp.spawn(train_catch_exception, nprocs=args["gpus_per_node"], args=(args,), join=True)
+        # start_processes(train_catch_exception, (args,), args["gpus_per_node"], True, False, start_method='spawn')
+        mp.spawn(train_catch_exception, nprocs=args["gpus_per_node"], args=(args,), join=True)
 
 
