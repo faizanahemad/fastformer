65b69d7c8bf486de2f52ee2e0bad3e4f2dd1253a
Faizan Ahemad <ahemf@amazon.com>
2021-05-26 20:27:51 +0530

    Add lm long

--- fastformer/training/train_lm_distributed.py
+++ fastformer/training/train_lm_distributed.py
@@ -278,15 +278,16 @@ class SuperGlueTest:
             from transformers.models.deberta import DebertaModel
             if os.path.exists(model):
                 model_name = model.split("/")[-1].split(".")[0]
-                model, tokenizer = get_mtt_backbone(model_name, 1, reinit=False)
-                model = model.to(self.device)
+                main_model, tokenizer = get_mtt_backbone(model_name, 1, reinit=False)
+                main_model = main_model.to(self.device)
                 state_dict = torch.load(model, map_location=self.device)
                 state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
                 try:
-                    model.load_state_dict(state_dict, strict=True)
+                    main_model.load_state_dict(state_dict, strict=True)
                 except:
                     state_dict = {k.replace("backbone.", ""): v for k, v in state_dict.items() if k.startswith("backbone.")}
-                    model.load_state_dict(state_dict, strict=True)
+                    main_model.load_state_dict(state_dict, strict=True)
+                model = main_model
             else:
 
                 tokenizer = AutoTokenizer.from_pretrained(model)
