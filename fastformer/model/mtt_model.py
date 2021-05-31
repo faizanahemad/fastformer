c7fb2d5386a9c95dae1ae7047c64d1af80fb46af
Faizan Ahemad <ahemf@amazon.com>
2021-05-31 19:06:09 +0530

    Old ConvBert

--- fastformer/model/mtt_model.py
+++ fastformer/model/mtt_model.py
@@ -19,7 +19,7 @@ import torch
 from torch import nn
 from torch.nn import CrossEntropyLoss, MSELoss
 from torch.nn import functional as F
-from transformers import AutoModel, RobertaTokenizerFast, ConvBertTokenizer
+from transformers import AutoModel, RobertaTokenizerFast, ConvBertTokenizer, ConvBertTokenizerFast
 
 from fastformer.model.fast_convbert import ConvBertModel, ConvBertConfig
 from fastformer.model.fastformer_vision_model import PatchCLR
@@ -92,6 +92,7 @@ def get_mtt_backbone(model_name, cls_tokens, reinit=False):
         # tokenizer = RobertaTokenizerFast.from_pretrained("roberta-base")
         # tokenizer = AutoTokenizer.from_pretrained("YituTech/conv-bert-base")
         # tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
+        # tokenizer = ConvBertTokenizerFast.from_pretrained("YituTech/conv-bert-base")
         tokenizer = ConvBertTokenizer.from_pretrained("YituTech/conv-bert-base")
         model = ConvBertModel(ConvBertConfig())
     else:
