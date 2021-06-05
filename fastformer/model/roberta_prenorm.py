e4d8dc062c532d04b1f8673475a442d4546c8664
Faizan Ahemad <ahemf@amazon.com>
2021-06-05 21:00:01 +0530

    Bug fix

--- fastformer/model/roberta_prenorm.py
+++ fastformer/model/roberta_prenorm.py
@@ -681,7 +681,7 @@ class RobertaEncoder(nn.Module):
             if grad_layer:
                 if approximate_unused_layers:
                     alpha = self.approximate_unused_layers_alpha
-                    hidden_states = alpha * scale_factor * temporary_hidden_state + (1 - alpha) * diff_factor * hidden_state_jump.detach()
+                    hidden_states = alpha * scale_factor * temporary_hidden_state + (1 - alpha) * diff_factor * hidden_state_jump
 
                 prev_grad_layer = i
                 hidden_state_jump = 0
