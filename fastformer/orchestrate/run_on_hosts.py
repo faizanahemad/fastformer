383c4968f48304fefcd6fb07b4ab2c4250611680
Faizan Ahemad <ahemf@amazon.com>
2021-06-18 20:02:31 +0530

    tricsk 1 4

--- fastformer/orchestrate/run_on_hosts.py
+++ fastformer/orchestrate/run_on_hosts.py
@@ -190,13 +190,13 @@ if __name__ == "__main__":
     lm_cmd = "python train_mtt_distributed.py -n %s -g 8 --nr %s --model_save_dir /home/ahemf/model_save_dir --model_save_name mtt.pth"
     lm_cmd += " --model_config prenorm-roberta-base"
     lm_cmd += " --cls_tokens 1"
-    lm_cmd += " --wandb_name base_drop_tricks_α0_sf1_40kW_@1_12_4@_1_1_50_0_m1"  #
+    lm_cmd += " --wandb_name base_drop_tricks_α0_sf1_20kW_@1_12_4@_1_1_50_0_m1"  #
     lm_cmd += " --dataset /home/ahemf/processed_datasets/all512"
     lm_cmd += " --log_every_steps 50 --num_workers 8 --save_every_steps 2000"
     # lm_cmd += " --wandb_dryrun"
     lm_cmd += " --init_method=tcp  --master_addr 0.0.0.0 --master_port 9998 --shuffle_dataset --accumulation_steps 1"
     lm_cmd += " --epochs 2 --lr 0.002 --batch_size 16 --lr_steps 1 --weight_decay 0.005"
-    lm_cmd += " --warmup_steps 40000 --gradient_clipping 3.0 --teacher_warmup_steps 1000"
+    lm_cmd += " --warmup_steps 20000 --gradient_clipping 3.0 --teacher_warmup_steps 1000"
     lm_cmd += " --sentence_order_prediction_w 1.0 --generator_w 1.0 --discriminator_w 50.0 --dino_w 0.0"  # --attention_penalty_w 100.0
     lm_cmd += " --optimizer adamw"
     lm_cmd += " --no_autocast"
@@ -207,6 +207,7 @@ if __name__ == "__main__":
     lm_cmd += " --start_from_proba 1.0"
     # lm_cmd += " --keep_last_layer"
     # lm_cmd += " --exclude_layers"
+    lm_cmd += " --lm_temperature 1.0"
     lm_cmd += " --beta_1 0.9 --beta_2 0.98"
     # lm_cmd += " --move_unused_to_cpu"
     # lm_cmd += " --pretrained_model roberta-base"
