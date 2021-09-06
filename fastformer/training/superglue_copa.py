e8d43e65e1e111d34ca2c31e03d310f42c3d17ed
Faizan Ahemad <ahemf@amazon.com>
2021-09-06 14:18:35 +0530

    super glue copa debug

--- fastformer/training/superglue_copa.py
+++ fastformer/training/superglue_copa.py
@@ -753,9 +753,9 @@ class SuperGlueTest:
         cosmos_qa = get_cosmos_qa(tokenizer)
         copa_pretrain = get_copa(tokenizer)
 
-        merged_pretrain = merge_datasets_as_df([scitail, hellaswag, cosmos_qa, swag, commonsense_qa, copa_pretrain], ["train", "validation"], ["label", "text"]).shuffle()
-        classifier_data = self.prepare_classifier(model_dict, merged_pretrain, device, 1, "merged_pretrain", rank, max_epochs=6)
-        _ = self.train_classifier(classifier_data["model"], device, classifier_data, max_epochs=6)
+        merged_pretrain = merge_datasets_as_df([copa_pretrain, scitail, hellaswag, cosmos_qa, swag, commonsense_qa, copa_pretrain], ["train", "validation"], ["label", "text"]).shuffle()
+        classifier_data = self.prepare_classifier(model_dict, merged_pretrain, device, 1, "merged_pretrain", rank, max_epochs=1)
+        _ = self.train_classifier(classifier_data["model"], device, classifier_data, max_epochs=1)
         model_dict["model"] = classifier_data["model"]
 
         copa_options = list(copa["train"]["choice1"]) + list(copa["train"]["choice2"]) + list(copa["validation"]["choice1"]) + list(
