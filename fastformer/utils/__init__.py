ca452a55c3607109b9414762e77e6de5d6ecc928
Faizan Ahemad <ahemf@amazon.com>
2021-09-09 22:54:23 +0530

    Incorporating confidence of predictions in locality

--- fastformer/utils/__init__.py
+++ fastformer/utils/__init__.py
@@ -1344,6 +1344,8 @@ class CoOccurenceModel(PreTrainedModel):
         word_ce = masked_lm_loss.detach().view(b, s) + 1.0
         word_ce_mins = word_ce.min(1).values.unsqueeze(-1)
         word_ce = 1 + ((word_ce - word_ce_mins) / (word_ce.max(1).values.unsqueeze(-1) - word_ce_mins)) * 4
+        word_ce = torch.exp(word_ce)
+        under_confidence_scores = torch.exp(under_confidence_scores)
 
         word_accuracy = None
         accuracy = None
