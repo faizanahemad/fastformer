17c5328729bc0215cedf6d1b8fe80625a4a62c2d
Faizan Ahemad <ahemf@amazon.com>
2021-09-10 17:18:18 +0530

    Incorporating confidence of predictions in locality

--- fastformer/filter_and_diversify/train_mlm_sop.py
+++ fastformer/filter_and_diversify/train_mlm_sop.py
@@ -562,8 +562,10 @@ class RTDMLMModel(PreTrainedModel):
         if validation_iter:
             word_wise_accuracy = mlm_rtd_hints["word_accuracy"]
             mask_locations = input_ids == self.tokenizer.mask_token_id
-            rtd_locations = torch.logical_and(input_ids != self.tokenizer.mask_token_id,
-                                              torch.logical_and(attention_mask, input_ids != label_mlm_input_ids))
+            rtd_locations = torch.logical_and(input_ids != label_mlm_input_ids, torch.logical_not(mask_locations))
+            rtd_locations_2 = torch.zeros_like(input_ids)
+            rtd_locations_2[rtd_mask[0], rtd_mask[1]] = 1.0
+            rtd_locations_2 = rtd_locations_2.bool()
             mask_accuracy = word_wise_accuracy[mask_locations].float().mean().item()
             # print(rtd_locations.size(), rtd_locations.sum(1).tolist())
             rtd_accuracy = word_wise_accuracy[rtd_locations].float().mean().item()
@@ -571,12 +573,13 @@ class RTDMLMModel(PreTrainedModel):
                   (input_ids[rtd_locations].view(-1) == label_mlm_input_ids[rtd_locations].view(-1)).float().mean().item(),
 
                   ((lm_predictions[mask_locations].view(-1) == label_mlm_input_ids[mask_locations].view(-1)).float().mean().item(),
-                  (lm_predictions[rtd_locations].view(-1) == label_mlm_input_ids[rtd_locations].view(-1)).float().mean().item()),
-                  (word_wise_accuracy[mask_locations].float().mean().item(), word_wise_accuracy[rtd_locations].float().mean().item(),),
+                  (lm_predictions[rtd_locations].view(-1) == label_mlm_input_ids[rtd_locations].view(-1)).float().mean().item(),
+                   (lm_predictions[rtd_locations_2].view(-1) == label_mlm_input_ids[rtd_locations_2].view(-1)).float().mean().item()),
+                  (word_wise_accuracy[mask_locations].float().mean().item(), word_wise_accuracy[rtd_locations].float().mean().item(), word_wise_accuracy[rtd_locations_2].float().mean().item()),
                   "\n",
                   # (input_ids[rtd_locations].view(-1) == label_mlm_input_ids[rtd_locations].view(-1)).long()
-                  input_ids[rtd_locations].view(-1), "\n",
-                  label_mlm_input_ids[rtd_locations].view(-1)
+                  # input_ids[rtd_locations].view(-1), "\n",
+                  # label_mlm_input_ids[rtd_locations].view(-1)
                   )
             rtd_post_replacement_accuracy = (top_k == label_mlm_input_ids).float().sum().item()
             accuracy = mlm_rtd_hints["accuracy"]
