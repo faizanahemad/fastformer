1e17785468d6dec341a121af1c5b9796e16b4a94
Faizan Ahemad <ahemf@amazon.com>
2021-09-17 00:02:21 +0530

    Speed

--- fastformer/training/superglue_copa.py
+++ fastformer/training/superglue_copa.py
@@ -424,12 +424,12 @@ class SuperGlueTest:
             if os.path.exists(model):
                 model_name = model.split("/")[-1].split(".")[0]
                 try:
-                    main_model, tokenizer = get_mtt_backbone(model_name, 1, self.enable_layer_normalizers, None, reinit=False,
-                                                             train_layer_normalizers=False, enable_layer_normalizers_statistics=self.enable_layer_normalizers,
+                    main_model, tokenizer = get_mtt_backbone(model_name, 1, False, None, reinit=False,
+                                                             train_layer_normalizers=False, enable_layer_normalizers_statistics=False,
                                                              dropout_prob=0.1)
                 except:
-                    main_model, tokenizer = get_mtt_backbone(model, 1, self.enable_layer_normalizers, None, reinit=False,
-                                                             train_layer_normalizers=False, enable_layer_normalizers_statistics=self.enable_layer_normalizers,
+                    main_model, tokenizer = get_mtt_backbone(model, 1, False, None, reinit=False,
+                                                             train_layer_normalizers=False, enable_layer_normalizers_statistics=False,
                                                              dropout_prob=0.1)
                 main_model = main_model.to(self.device)
                 state_dict = torch.load(model, map_location=self.device)
