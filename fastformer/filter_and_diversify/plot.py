37240728a9739f477c53c3e25b5ad81f10e5d1b3
Faizan Ahemad <ahemf@amazon.com>
2021-10-19 20:15:42 +0530

    Bug fix

--- fastformer/filter_and_diversify/plot.py
+++ fastformer/filter_and_diversify/plot.py
@@ -7,6 +7,7 @@ argv = list(sys.argv)
 from IPython.terminal.embed import InteractiveShellEmbed
 my_shell = InteractiveShellEmbed()
 result = my_shell.getoutput("pip install uniplot")
+print(result)
 result = my_shell.getoutput("cat output.log | grep %s | awk '{print $NF}'" % (argv[1]))
 
 from uniplot import plot
